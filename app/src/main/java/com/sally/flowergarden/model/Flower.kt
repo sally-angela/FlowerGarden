9058e21794ade4bb473a59a5a56f13e95260bc16
sally-angela <118360311+sally-angela@users.noreply.github.com>
2024-03-31 15:25:07 +0700

    add bottom menu and drawer, update ui home

---
+++ app/src/main/java/com/sally/flowergarden/model/Flower.kt
@@ -0,0 +1,10 @@
+package com.sally.flowergarden.model
+
+data class Flower(
+    val id:Int?,
+    val title:String?,
+    val author:String?,
+    val description:String,
+    val paragraphs:List<String>?,
+    val images:String?
+)
--- app/src/main/java/com/sally/flowergarden/view/MainActivity.kt
+++ app/src/main/java/com/sally/flowergarden/view/MainActivity.kt
@@ -2,13 +2,27 @@ package com.sally.flowergarden.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.sally.flowergarden.R
 import com.sally.flowergarden.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout) || super.onSupportNavigateUp()
+    }
+
 }
