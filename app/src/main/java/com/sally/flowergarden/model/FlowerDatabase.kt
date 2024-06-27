77941d99d29e101bfa03d99fccbe0ec36034f6d4
tygarean <nico.victorio@gmail.com>
2024-06-27 14:14:42 +0700

    add migration

--- app/src/main/java/com/sally/flowergarden/model/FlowerDatabase.kt
+++ app/src/main/java/com/sally/flowergarden/model/FlowerDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sally.flowergarden.util.DB_NAME
+import com.sally.flowergarden.util.MIGRATION_1_2
 
-@Database(entities = [Flower::class, User::class], version =  1)
+@Database(entities = [Flower::class, User::class], version = 2)
 
 abstract class FlowerDatabase:RoomDatabase() {
     abstract fun flowerDao(): FlowerDao
@@ -19,6 +20,7 @@ abstract class FlowerDatabase:RoomDatabase() {
         fun buildDatabase(context: Context) = Room.databaseBuilder(
             context.applicationContext,
             FlowerDatabase::class.java, DB_NAME)
+            .addMigrations(MIGRATION_1_2)
             .build()
 
         operator fun invoke(context: Context) {
--- app/src/main/java/com/sally/flowergarden/util/Util.kt
+++ app/src/main/java/com/sally/flowergarden/util/Util.kt
@@ -2,6 +2,8 @@ package com.sally.flowergarden.util
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.sally.flowergarden.model.FlowerDatabase
 
 val DB_NAME = "flowerdb"
@@ -9,4 +11,11 @@ val DB_NAME = "flowerdb"
 fun buildDb(context: Context): FlowerDatabase {
     val db = Room.databaseBuilder(context, FlowerDatabase::class.java, DB_NAME).build()
     return db
+}
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE flower ADD COLUMN images TEXT DEFAULT 'https://loremflickr.com/320/240/flower?lock=1' not null")
+    }
 }
